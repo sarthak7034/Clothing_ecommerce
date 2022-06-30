162fadbab712d5cf3517608b73da4d0b50f0aa85
sarthak7034 <sarthak_660027@yahoo.co.in>
2022-06-30 16:55:43 +0200

    'sign-up-complete'

--- src/utils/firebase/firebase.utils.js
+++ src/utils/firebase/firebase.utils.js
@@ -5,6 +5,7 @@ import {
   signInWithPopup,
   GoogleAuthProvider,
   createUserWithEmailAndPassword,
+  signInWithEmailAndPassword
 } from 'firebase/auth';
 
 import {
@@ -75,10 +76,16 @@ export  const createUserDocumentFromAuth = async (userAuth, additionalInformatio
   return userDocRef;
 }
 
-export const createAuthUserWithEmailAndPassword = async (email, password) =>{
- 
+export const createAuthUserWithEmailAndPassword = async (email, password) =>{ 
   if(!email || !password) return;
 
   return await createUserWithEmailAndPassword(auth, email, password);
+
+}
+
+export const signInAuthUserWithEmailAndPassword = async (email, password) =>{ 
+  if(!email || !password) return;
+
+  return await signInWithEmailAndPassword(auth, email, password);
 
 }
